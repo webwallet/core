76a3aa1dfbda6578e1c064ca04cc402a22f7b723
Jorge Zaccaro <boolpath@gmail.com>
2022-03-09 23:00:47 -0500

    feat: generate signature from private key pair

--- src/webwallet/crypto/bitcoinj/keys.clj
+++ src/webwallet/crypto/bitcoinj/keys.clj
@@ -1,13 +1,19 @@
 (ns webwallet.crypto.bitcoinj.keys
   (:require [webwallet.crypto.hashing :as hashing])
-  (:import (org.bitcoinj.core ECKey Utils Base58)))
+  (:import (org.bitcoinj.core ECKey Utils Base58 Sha256Hash)))
+
+(defn bytes->hex [bytes]
+  (.encode Utils/HEX bytes))
+
+(defn hex->bytes [hex]
+  (.decode Utils/HEX hex))
 
 (defn keypair-from-private-hex [private-hex]
   (-> (BigInteger. private-hex 16)
       (ECKey/fromPrivate)))
 
 (defn keypair-from-public-hex [public-hex]
-  (-> (.decode Utils/HEX public-hex)
+  (-> (hex->bytes public-hex)
       (ECKey/fromPublicOnly)))
 
 (defn get-private-key-hex [keypair]
@@ -18,20 +24,26 @@
 
 (defn public-from-private-hex
   ([private-hex]
-   (public-from-private-string private-hex true))
+   (public-from-private-hex private-hex true))
   ([private-hex compressed]
    (-> (BigInteger. private-hex 16)
-       (ECKey/publicKeyFromPrivate compressed))))
+       (ECKey/publicKeyFromPrivate compressed)
+       (bytes->hex))))
 
 (defn address-from-keypair [keypair]
   (let [prefix-integer (Integer/parseInt "00" 16)]
-    (->> keypair
-         (.getPubKeyHash)
+    (->> (.getPubKeyHash keypair)
          (Base58/encodeChecked prefix-integer))))
 
 (defn address-from-public-hex [public-hex]
   (let [prefix-integer (Integer/parseInt "00" 16)]
-    (->> public-hex
-         (.decode Utils/HEX)
+    (->> (hex->bytes public-hex)
          (Utils/sha256hash160)
          (Base58/encodeChecked prefix-integer))))
+
+(defn hash-twice-and-sign [message keypair]
+  (->> (.getBytes message)
+       (Sha256Hash/twiceOf)
+       (.sign keypair)
+       (.encodeToDER)
+       (bytes->hex)))
--- test/webwallet/crypto/bitcoinj/keys_test.clj
+++ test/webwallet/crypto/bitcoinj/keys_test.clj
@@ -6,6 +6,8 @@
 (def private-key-hex "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725")
 (def public-key-hex "0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352")
 (def address-base58 "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs")
+(def message-to-sign "hello")
+(def message-signature "30450221009b82b660de94b623a1d99bbe11d36733e0fb8a510cbafde3101a7d2739c3a917022008c12dd6379afb027ebc271c608187770cfa1dc4e2bc75eeba1e3c5271bc6609")
 
 ;; "key pair" is an object
 (deftest keypair-generation
@@ -24,3 +26,9 @@
           address-from-public (btcj-keys/address-from-public-hex public-key-hex)]
       (is (= address-from-keypair address-base58))
       (is (= address-from-public address-base58)))))
+
+(deftest signature-generation
+  (testing "Generate a signature starting from a private key string"
+    (let [keypair-from-private (btcj-keys/keypair-from-private-hex private-key-hex)
+          generated-signature (btcj-keys/hash-twice-and-sign message-to-sign keypair-from-private)]
+      (is (= generated-signature message-signature)))))
