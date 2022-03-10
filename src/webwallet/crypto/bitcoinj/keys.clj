c4a08d7d3780a4c93b92dc85cb8b7785fbe8084c
Jorge Zaccaro <boolpath@gmail.com>
2022-03-09 20:11:42 -0500

    feat: address and key pair generation

--- src/webwallet/crypto/bitcoinj/keys.clj
+++ src/webwallet/crypto/bitcoinj/keys.clj
@@ -1,11 +1,37 @@
 (ns webwallet.crypto.bitcoinj.keys
-  (:require [buddy.core.codecs :refer [bytes->hex]])
-  (:import (org.bitcoinj.core ECKey)))
+  (:require [webwallet.crypto.hashing :as hashing])
+  (:import (org.bitcoinj.core ECKey Utils Base58)))
 
-(defn public-from-private-string
-  ([private-string]
-   (public-from-private-string private-string true))
-  ([private-string compressed]
-   (let [biginteger-from-hex (BigInteger. private-string 16)
-         public-from-private (ECKey/publicKeyFromPrivate biginteger-from-hex compressed)]
-     (bytes->hex public-from-private))))
+(defn keypair-from-private-hex [private-hex]
+  (-> (BigInteger. private-hex 16)
+      (ECKey/fromPrivate)))
+
+(defn keypair-from-public-hex [public-hex]
+  (-> (.decode Utils/HEX public-hex)
+      (ECKey/fromPublicOnly)))
+
+(defn get-private-key-hex [keypair]
+  (.getPrivateKeyAsHex keypair))
+
+(defn get-public-key-hex [keypair]
+  (.getPublicKeyAsHex keypair))
+
+(defn public-from-private-hex
+  ([private-hex]
+   (public-from-private-string private-hex true))
+  ([private-hex compressed]
+   (-> (BigInteger. private-hex 16)
+       (ECKey/publicKeyFromPrivate compressed))))
+
+(defn address-from-keypair [keypair]
+  (let [prefix-integer (Integer/parseInt "00" 16)]
+    (->> keypair
+         (.getPubKeyHash)
+         (Base58/encodeChecked prefix-integer))))
+
+(defn address-from-public-hex [public-hex]
+  (let [prefix-integer (Integer/parseInt "00" 16)]
+    (->> public-hex
+         (.decode Utils/HEX)
+         (Utils/sha256hash160)
+         (Base58/encodeChecked prefix-integer))))
--- test/webwallet/crypto/bitcoinj/keys_test.clj
+++ test/webwallet/crypto/bitcoinj/keys_test.clj
@@ -1,12 +1,26 @@
 (ns webwallet.crypto.bitcoinj.keys-test
   (:require [clojure.test :refer :all]
-            [webwallet.crypto.bitcoinj.keys :refer :all]))
+            [webwallet.crypto.bitcoinj.keys :as btcj-keys]))
 
 ;; https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
-(def private-key-string "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725")
-(def public-key-string "0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352")
+(def private-key-hex "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725")
+(def public-key-hex "0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352")
+(def address-base58 "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs")
 
+;; "key pair" is an object
 (deftest keypair-generation
-  (testing "Generate public key string from private key string"
-    (let [public-from-private (public-from-private-string private-key-string)]
-      (is (= public-from-private public-key-string)))))
+  (testing "Generate key pair from private key string"
+    (let [keypair-from-private (btcj-keys/keypair-from-private-hex private-key-hex)
+          private-from-keypair (btcj-keys/get-private-key-hex keypair-from-private)
+          public-from-keypair (btcj-keys/get-public-key-hex keypair-from-private)]
+      (is (= private-from-keypair private-key-hex))
+      (is (= public-from-keypair public-key-hex))))
+  (testing "Derive public key from private key string"
+    (let [public-from-private (btcj-keys/public-from-private-hex private-key-hex)]
+      (is (= public-from-private public-key-hex))))
+  (testing "Generate address from public key string"
+    (let [keypair-from-public (btcj-keys/keypair-from-public-hex public-key-hex)
+          address-from-keypair (btcj-keys/address-from-keypair keypair-from-public)
+          address-from-public (btcj-keys/address-from-public-hex public-key-hex)]
+      (is (= address-from-keypair address-base58))
+      (is (= address-from-public address-base58)))))
